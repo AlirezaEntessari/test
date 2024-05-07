6b7cd991aa17408dfc83563af5b16efeb69c90d7
Alireza Entessari <alintssr@gmail.com>
2024-05-07 13:07:20 -0400

    Updated pg credentials to connect to AWS EC2 instance

--- server/db.js
+++ server/db.js
@@ -3,9 +3,9 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "rootroot",
-    host: "localhost",
+    host: "database-test1.c5qao8u6wxjb.us-east-2.rds.amazonaws.com",
     port: 5432,
-    database: "test"
+    database: "test_database"
 })
 
 module.exports = pool;
